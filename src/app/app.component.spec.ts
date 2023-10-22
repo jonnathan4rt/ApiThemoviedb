e87bb7fc25bd4c89218cad61e7ffda7309278220
jonnathan4rt <jonnathan.rog@gmail.com>
2023-10-21 22:29:37 -0500

    Consumo total del API

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shinyFlix'`, () => {
+  it(`should have as title 'ngFlix'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shinyFlix');
+    expect(app.title).toEqual('ngFlix');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shinyFlix app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ngFlix app is running!');
   });
 });
---
+++ src/app/services/movies-service.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MoviesService } from './movies.service';
+
+
+describe('MoviesServiceService', () => {
+  let service: MoviesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MoviesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
